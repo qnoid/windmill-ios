95a9b919008792473b42708568933d0f4286f5d0
Markos Charatzas <markos@qnoid.com>
2016-05-04 22:10:38 +0100

    init

---
+++ windmillTests/windmillTests.swift
@@ -0,0 +1,36 @@
+//
+//  windmillTests.swift
+//  windmillTests
+//
+//  Created by Markos Charatzas on 04/05/2016.
+//  Copyright © 2016 Windmill. All rights reserved.
+//
+
+import XCTest
+@testable import windmill
+
+class windmillTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
