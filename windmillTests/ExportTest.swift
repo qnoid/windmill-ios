78d60586e7344518b70d33d882a9f144670433d4
Markos Charatzas <markos@qnoid.com>
2019-05-09 14:01:46 +0300

    ~ fixed source code for tests

--- windmillTests/ExportTest.swift
+++ windmillTests/ExportTest.swift
@@ -14,21 +14,21 @@ class ExportTest: XCTestCase {
 
     func testGivenSameVersionAssertIsCompatibleTrue() {
         let inTen: Date = Date().addingTimeInterval(10)
-        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91"), deployment: BuildSettings.Deployment( target: "12.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
+        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91", date: Date()), deployment: BuildSettings.Deployment( target: "12.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
         
         XCTAssertTrue(metadata.targetsEqualOrLowerThan(version: "12.0"))
     }
 
     func testGivenSystemVersionIsLessAssertIsCompatibleFalse() {
         let inTen: Date = Date().addingTimeInterval(10)
-        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91"), deployment: BuildSettings.Deployment( target: "12.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
+        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91", date: Date()), deployment: BuildSettings.Deployment( target: "12.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
         
         XCTAssertFalse(metadata.targetsEqualOrLowerThan(version: "11.0"))
     }
     
     func testGivenSystemVersionIsGreaterAssertIsCompatibleTrue() {
         let inTen: Date = Date().addingTimeInterval(10)
-        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91"), deployment: BuildSettings.Deployment( target: "11.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
+        let metadata = Export.Metadata(configuration: .release, commit: Repository.Commit(branch: "master", shortSha: "95a9b91", date: Date()), deployment: BuildSettings.Deployment( target: "11.0"), distributionSummary: DistributionSummary(certificateExpiryDate: inTen), applicationProperties: ApplicationProperties(bundleDisplayName: "1.0", bundleVersion: "1.0"))
         
         XCTAssertTrue(metadata.targetsEqualOrLowerThan(version: "12.0"))
     }
